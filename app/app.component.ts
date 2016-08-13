949db3346d09421ec34ad4cde6a4a1290ef26255
Kevin Lang <klang2012@gmail.com>
2016-08-12 18:44:49 -0700

    hero data access out to service

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,19 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { Hero } from './hero';
 
-const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
-];
+import { HeroService } from './hero.service';
 
 @Component({
   selector: 'my-app',
@@ -77,15 +66,27 @@ styles: [`
     height: 1.8em;
     margin-right: .8em;
     border-radius: 4px 0 0 4px;
-  }`]
+  }`],
+  providers: [HeroService]
 })
 
-export class AppComponent { 
+export class AppComponent implements OnInit { 
   title = 'Tour of heroes';
   selectedHero: Hero;
-  heroes = HEROES;
+  heroes: Hero[];
+
+  constructor(private heroService: HeroService) {
+  }
+
+  ngOnInit() {
+    this.getHeroes();
+  }
 
   onSelect(hero: Hero) {
     this.selectedHero = hero;
   };
+
+  getHeroes() {
+    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+  }
 }
