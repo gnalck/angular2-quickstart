1c835ebd5c1fd8b6f9ca8722d040c52d5ad47bf0
Kevin Lang <klang2012@gmail.com>
2016-08-13 09:25:05 -0700

    routes and moving out styles/html to new files

--- app/app.module.ts
+++ app/app.module.ts
@@ -7,12 +7,14 @@ import { routing } from './app.routing';
 
 import { HeroesComponent } from './heroes.component'
 import { HeroDetailComponent } from './hero-detail.component';
+import { DashboardComponent } from './dashboard.component';
 
 import { HeroService } from './hero.service';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule, routing ],
-  declarations: [ AppComponent, HeroesComponent, HeroDetailComponent ],
+  declarations: [ AppComponent, DashboardComponent, 
+                  HeroesComponent, HeroDetailComponent ],
   bootstrap:    [ AppComponent ],
   providers:    [ HeroService ]
 })
--- app/app.routing.ts
+++ app/app.routing.ts
@@ -1,11 +1,26 @@
 import { Routes, RouterModule } from '@angular/router';
 
 import { HeroesComponent } from './heroes.component';
+import { HeroDetailComponent } from './hero-detail.component';
+import { DashboardComponent } from './dashboard.component';
 
 const appRoutes: Routes = [
   {
     path: 'heroes',
     component: HeroesComponent
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent
+  },
+  {
+    path: 'detail/:id',
+    component: HeroDetailComponent
+  },
+  {
+    path: '',
+    redirectTo: 'dashboard',
+    pathMatch: 'full' // what does this do?
   }
 ];
 
